a01d848f45a4d752f3ed271a9ae03191adb67e3d
강두희 <rkdengml0920@naver.com>
2024-07-30 11:48:44 +0900

    한손 다이어리 - 기본 css 완료, 가입js 완료

---
+++ js/join.js
@@ -0,0 +1,29 @@
+(function () {
+  const joinCon = document.querySelector(".join-container");
+  const joinForm = document.querySelector(".join-form");
+  const joinValue = joinForm.querySelector("input");
+  const mainWrap = document.querySelector(".wrap");
+  const greeting = document.querySelector(".header-greeting");
+
+  function greetingUser(elem) {
+    mainWrap.classList.remove("hidden");
+    greeting.innerText = `hello, ${elem}!`;
+  }
+
+  function joinHandler(e) {
+    e.preventDefault();
+    const userName = joinValue.value;
+    localStorage.setItem("userName", userName);
+    joinCon.classList.add("hidden");
+    greetingUser(userName);
+  }
+
+  const localUserName = localStorage.getItem("userName");
+
+  if (localUserName) {
+    greetingUser(localUserName);
+  } else {
+    joinCon.classList.remove("hidden");
+    joinForm.addEventListener("submit", joinHandler);
+  }
+})();
